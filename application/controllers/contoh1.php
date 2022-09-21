a276842c882481b26737ffaff43d6a1adc1670a8
MuhReihanF <muhreihanf12@gmail.com>
2022-09-22 01:13:08 +0700

    menambahkan contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Reihan fitriansyah, saya tinggal di kabupaten Kayong Utara, olahraga yang saya sukai adalah basket";
+    }
+}
