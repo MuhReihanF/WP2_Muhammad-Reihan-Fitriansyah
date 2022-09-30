e538a381f6b1f00f53294591bb3af3f5d086b857
MuhReihanF <muhreihanf12@gmail.com>
2022-09-30 21:36:21 +0700

    menambahkan tugas 3 webprog

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php   echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhammad Reihan</li>
+        <li>Nama Belakang : Fitriansyah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Re. Mathadinata, komplek pemda, gang alpokat</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Makassar</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Pencak Silat</li>
+        <li>Basket</li>
+    </ul>
+</section>
