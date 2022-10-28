82a3af93069dd8fb5d406463878079dcbfd4b5ca
MuhReihanF <muhreihanf12@gmail.com>
2022-10-28 18:31:11 +0700

    tugas 7

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,8 +12,10 @@ class Admin extends CI_Controller
     public function index()
     {
         $data['judul'] = 'Dashboard';
-        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-        $data['anggota'] = $this->ModelUser->getUserLimit()->result_array();
+        $data['user'] = $this->ModelUser->cekData(['email' => $this-
+>session->userdata('email')])->row_array();
+        $data['anggota'] = $this->ModelUser->getUserLimit()-
+>result_array();
         $data['buku'] = $this->ModelBuku->getBuku()->result_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
